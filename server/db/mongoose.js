28664365f8d9af9ec23ac29bb135d293a6714c70
EJ Ozyazgan <ejozyazgan@gmail.com>
2018-06-22 16:33:59 -0700

    Setup test and dev envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://root:Demon5310!@ds115931.mlab.com:15931/node-course-todo' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://root:Demon5310!@ds115931.mlab.com:15931/node-course-todo');
 
 module.exports = {mongoose};
